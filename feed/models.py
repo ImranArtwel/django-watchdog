afde99016eb0626864ac787f2ec18321420bcfbb
Artwel Junior <artwelljunior@gmail.com>
2016-11-12 05:17:03 +0000

    feedcommit

---
+++ feed/models.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    author = models.ForeignKey(User,related_name='feed_post')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
