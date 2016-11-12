264d788d12273e750822681f2f9e8bb6e5af9eea
Artwel Junior <artwelljunior@gmail.com>
2016-11-12 06:29:41 +0000

    cloud 9 app

--- feed/admin.py
+++ feed/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display =['title','author','created']
+    search_fields =['title']
+
+admin.site.register(Post,PostAdmin)
