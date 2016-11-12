bcafad4047d63b70c8b9efdf222d4498d3690950
Artwel Junior <artwelljunior@gmail.com>
2016-11-12 08:25:03 +0000

    feed with sync db

--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from .forms import PostForm
+from .models import Post
 # Create your views here.
+def index(request):
+    recent_posts_list =Post.objects.order_by('-created')[:10]
+    form = PostForm()
+    user = request.user
+    context = {'recent_posts_list': recent_posts_list,'form': form}
+    
+    if request.method =="POST":
+        form =PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+    else:
+        form = PostForm()
+    return render(request,'index.html', context)
